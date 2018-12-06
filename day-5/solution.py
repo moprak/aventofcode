402d4a4550616bcc7033bde8be32aab5b9f34c45
Prakash Mohan <mopraks@gmail.com>
2018-12-06 05:36:52 -0600

    day 5 solution

---
+++ day-5/solution.py
@@ -0,0 +1,20 @@
+from string import ascii_lowercase
+f = open('input','r')
+data = f.read().strip()
+f.close()
+
+def react(data):
+    pre = ''
+    post = data
+    while(len(post) != len(pre)):
+        pre = post
+        for l in ascii_lowercase:
+            post = post.replace(l+l.upper(), '')
+            post = post.replace(l.upper()+l, '')
+    return(len(post))
+
+print('Part A: ', react(data))
+scores = {}
+for l in ascii_lowercase:
+    scores[l] = react(data.replace(l,'').replace(l.upper(), ''))
+print('Part B: ', min(scores.values()))
