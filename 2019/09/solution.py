cf17b3aa58a62054bb0dbf0abc950b92b56dbd77
Prakash Mohan <mopraks@gmail.com>
2020-11-25 23:50:14 -0600

    update intcode for day 9, add day 9 solution

---
+++ 2019/09/solution.py
@@ -0,0 +1,23 @@
+import numpy as np
+import re
+from collections import Counter,defaultdict, deque
+import itertools
+from string import ascii_uppercase, ascii_lowercase
+from copy import deepcopy
+from blist import blist
+from aocd import get_data
+from functools import reduce
+from aocd.models import Puzzle
+import intcode
+
+puzzle = Puzzle(year=2019, day=9)
+raw = puzzle.input_data
+data = [int(i) for i in raw.strip('\n').split(sep=',')]
+
+program = intcode.Intcode(data,[1])
+program.run()
+puzzle.answer_a = program.outputs.pop()
+
+program = intcode.Intcode(data,[2])
+program.run()
+puzzle.answer_b = program.outputs.pop()
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Intcode:
     def __init__(self, program, inputs = []):
         self.program = program[:]
@@ -7,58 +8,77 @@ class Intcode:
         self.input_count = 0
         self.produced_output = False
         self.halted = False
+        self.memory = {}
+        self.relative_base = 0
     def add_input(self, d):
         self.inputs = [d] + self.inputs
     def get(self, idx):
-        return self.program[idx]
+        if idx < len(self.program):
+            return self.program[idx]
+        return self.memory.get(idx,0)
     def set(self, idx, value):
-        self.program[idx] = value
+        if idx < len(self.program):
+            self.program[idx] = value
+        else:
+            self.memory[idx] = value
     def get_params(self, ptr, nargs):
-            params = [ self.ptr+i+1 if (self.program[self.ptr]//int(10**(2+i)))%10 else self.program[self.ptr+i+1] for i in range(nargs)]
-            return params
+        params = []
+        for i in range(nargs):
+            mode = (self.get(self.ptr)//int(10**(2+i)))%10
+            if mode == 1:
+                params.append(self.ptr+i+1)
+            elif mode == 2:
+                params.append(self.get(self.ptr + i + 1) + self.relative_base)
+            else:
+                params.append(self.get(self.ptr + i + 1))
+        return params
     def run(self):
         while not self.halted and not self.produced_output:
-            opcode = self.program[self.ptr]%100
+            opcode = self.get(self.ptr)%100
             if opcode == 99:
                 self.halted = True
             elif opcode == 1:
                 params = self.get_params(self.ptr, 3)
-                self.program[params[2]] = self.program[params[0]] + self.program[params[1]]
+                self.set(params[2] , self.get(params[0]) + self.get(params[1]))
                 self.ptr += 4
             elif opcode == 2:
                 params = self.get_params(self.ptr, 3)
-                self.program[params[2]] = self.program[params[0]] * self.program[params[1]]
+                self.set(params[2] , self.get(params[0]) * self.get(params[1]))
                 self.ptr += 4
             elif opcode == 3:
                 params = self.get_params(self.ptr, 1)
                 self.input_count += 1
                 if self.inputs:
-                    self.program[params[0]] = self.inputs.pop()
+                    self.set(params[0] , self.inputs.pop())
                 else:
-                    self.program[params[0]] = int(input('enter input: '))
+                    self.set(params[0] , int(input('enter input: ')))
                 self.ptr +=2
             elif opcode == 4:
                 params = self.get_params(self.ptr, 1)
-                self.outputs.append(self.program[params[0]])
+                self.outputs.append(self.get(params[0]))
                 self.produced_output = True
                 self.ptr +=2
             elif opcode == 5:
                 params = self.get_params(self.ptr, 2)
-                if self.program[params[0]]:
-                    self.ptr = self.program[params[1]]
+                if self.get(params[0]):
+                    self.ptr = self.get(params[1])
                 else:
                     self.ptr +=3
             elif opcode == 6:
                 params = self.get_params(self.ptr, 2)
-                if not self.program[params[0]]:
-                    self.ptr = self.program[params[1]]
+                if not self.get(params[0]):
+                    self.ptr = self.get(params[1])
                 else:
                     self.ptr +=3
             elif opcode == 7:
                 params = self.get_params(self.ptr, 3)
-                self.program[params[2]] = int(self.program[params[0]] < self.program[params[1]])
+                self.set(params[2] , int(self.get(params[0]) < self.get(params[1])))
                 self.ptr += 4
             elif opcode == 8:
                 params = self.get_params(self.ptr, 3)
-                self.program[params[2]] = int(self.program[params[0]] == self.program[params[1]])
+                self.set(params[2] , int(self.get(params[0]) == self.get(params[1])))
                 self.ptr += 4
+            elif opcode == 9:
+                params = self.get_params(self.ptr, 1)
+                self.relative_base += self.get(params[0])
+                self.ptr += 2
