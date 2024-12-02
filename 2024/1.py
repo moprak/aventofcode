fdbcdc1d2e79a4d30581e37b0609ef7ed7de9254
Prakash Mohan <mopraks@gmail.com>
2024-12-01 18:05:52 -0700

    better use of numpy

--- 2024/1.py
+++ 2024/1.py
@@ -3,12 +3,8 @@ from collections import Counter
 import numpy as np
 from aocd.models import Puzzle
 
-from utils import *
-
 puzzle = Puzzle(year=2024, day=1)
-raw = puzzle.input_data
-data = [ints(i) for i in raw.splitlines()]
-data = np.array(data)
+data = np.loadtxt(puzzle.input_data_path, dtype=int)
 p1 = np.abs(np.sort(data[:, 0]) - np.sort(data[:, 1])).sum()
 c = Counter(data[:, 1])
 p2 = sum(i * c[i] for i in data[:, 0])
